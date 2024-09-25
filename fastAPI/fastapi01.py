42e9d6c3648f2631238ec8ded1b8b88f99df276d
Emine Tas <eminetas126@gmail.com>
2024-09-25 12:09:41 +0300

    fastAPI

---
+++ fastAPI/fastapi01.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 12:01:08 2024
+
+@author: Asus
+"""
+
+import fastapi
+from fastapi import FastAPI
+"""
+yukarda fastapi import edip FastAPI classını yüklüyoruz
+ilk versiyon numarasnın yazdırıyoruz
+kullanılan paketleri yazdırıyoruz
+directory yazdırıyoruz
+FastAPI kendisi hakkınnda bilgileri yazdırıyoruz
+"""
+print(fastapi.__version__)
+print(help(fastapi))
+print(dir(fastapi))
+print(help(FastAPI))
