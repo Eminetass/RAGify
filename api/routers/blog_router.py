54b4a6dcb23d20b7c07058b29b606878e18059c1
Emine Taş <138786353+Eminetass@users.noreply.github.com>
2024-10-13 14:58:07 +0300

    Update blog_router.py

--- api/routers/blog_router.py
+++ api/routers/blog_router.py
@@ -1,10 +1,14 @@
+#FasAPI dan gerekli modülller,Session sınıfını içe aktarma,fonksiyon içe katarmalaır yapılıyor.
 from fastapi import APIRouter, HTTPException , Depends
 from sqlalchemy.orm import Session
 from api.services.blog_service import create_embedding
 from models.models import get_db
 
+#yeni bir API router'ı oluşturuluyor.
 router = APIRouter()
 
+
+#POST isteği için bir API rotası tanımlanıyor.Fonksiyon tanımlanıyor. Kullanıcıdan alınan metni embedding'e dönüştürür ve sonucu döndürür.
 @router.post("/create_embedding/")
 def create_embedding_route(text: str, db: Session = Depends(get_db)):
     try:
